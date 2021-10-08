19e1a947979aa43d76c96116780f8866676ac885
Chih-Wei Fang <dodo0822@gmail.com>
2021-10-07 22:52:40 -0700

    feat(backend): update Dockerfile and DB schema

--- backend/sgmp/models/analytics.py
+++ backend/sgmp/models/analytics.py
@@ -2,7 +2,7 @@ from models.shared import db
 
 class Analytics(db.Model):
     analytics_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256), nullable=False)
+    name = db.Column(db.String(256), nullable=False, index=True)
     description = db.Column(db.Text(), nullable=False)
     formula = db.Column(db.Text(), nullable=False)
 
