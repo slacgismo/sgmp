0ea660dcae7c27ee4c386a4499a5811ae9145647
Chih-Wei Fang <dodo0822@gmail.com>
2021-10-21 19:30:43 -0700

    feat(frontend): update DeviceList

--- frontend/src/views/devices/DeviceList.vue
+++ frontend/src/views/devices/DeviceList.vue
@@ -290,6 +290,23 @@
       </table>
     </div>
   </div>
+  
+  <Dialog :open="isOpen" @close="setIsOpen">
+    <DialogOverlay />
+
+    <DialogTitle>Deactivate account</DialogTitle>
+    <DialogDescription>
+      This will permanently deactivate your account
+    </DialogDescription>
+
+    <p>
+      Are you sure you want to deactivate your account? All of your data will be
+      permanently removed. This action cannot be undone.
+    </p>
+
+    <button @click="setIsOpen(false)">Deactivate</button>
+    <button @click="setIsOpen(false)">Cancel</button>
+  </Dialog>
 </template>
 
 <script>
