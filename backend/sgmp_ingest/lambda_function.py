a0fb7f8bb71ff80e245d61489485a32e64f27cf5
Chih-Wei Fang <dodo0822@gmail.com>
2021-10-28 23:33:11 -0700

    feat(backend): update ingest code to add events

--- backend/sgmp_ingest/lambda_function.py
+++ backend/sgmp_ingest/lambda_function.py
@@ -1,6 +1,7 @@
 import psycopg2
 import psycopg2.extras
 import os
+import json
 
 pg_host = os.environ['PG_HOST']
 pg_user = os.environ['PG_USER']
@@ -8,6 +9,7 @@ pg_pass = os.environ['PG_PASS']
 pg_database = os.environ['PG_DATABASE']
 
 sql_house = 'INSERT INTO house_data (timestamp, house_id, device_name, field, value_decimal, value_text) VALUES %s'
+sql_event = 'INSERT INTO events (timestamp, house_id, device_name, "type", "data") VALUES (to_timestamp(%s), %s, %s, %s, %s)'
 
 # Function reference: https://stackoverflow.com/questions/6027558/flatten-nested-dictionaries-compressing-keys
 def flatten(d, parent_key='') -> dict:
@@ -34,25 +36,41 @@ def lambda_handler(event, context):
         user=pg_user,
         password=pg_pass)
 
-    # Extract payload
-    timestamp = float(event['timestamp']) / 1000
-    device_name = event['device_name']
-    client_id = event['client_id']
-    house_id = int(client_id.split('_')[-1])
+    if 'topic' in event and event['topic'] == 'event':
+        # Extract payload
+        timestamp = float(event['timestamp']) / 1000
+        device_name = event['device_name']
+        client_id = event['client_id']
+        house_id = int(client_id.split('_')[-1])
+        type = event['data']['type']
+        data = json.dumps(event['data']['data'])
+        cur = conn.cursor()
+        cur.execute(sql_event, (timestamp, house_id, device_name, type, data))
+        conn.commit()
+        cur.close()
+        
+        return 'Event %s, device_id=%d, timestamp=%d' % (type, event['device_id'], event['timestamp'])
+        
+    else:
+        # Extract payload
+        timestamp = float(event['timestamp']) / 1000
+        device_name = event['device_name']
+        client_id = event['client_id']
+        house_id = int(client_id.split('_')[-1])
 
-    fields = flatten(event['data'])
+        fields = flatten(event['data'])
 
-    data_house = []
+        data_house = []
 
-    for field, value in fields.items():
-        if isinstance(value, float) or isinstance(value, int):
-            data_house.append((timestamp, house_id, device_name, field, value, None))
-        else:
-            data_house.append((timestamp, house_id, device_name, field, None, value))
+        for field, value in fields.items():
+            if isinstance(value, float) or isinstance(value, int):
+                data_house.append((timestamp, house_id, device_name, field, value, None))
+            else:
+                data_house.append((timestamp, house_id, device_name, field, None, value))
 
-    cur = conn.cursor()
-    psycopg2.extras.execute_values(cur, sql_house, data_house, template='(to_timestamp(%s), %s, %s, %s, %s, %s)')
-    conn.commit()
-    cur.close()
-    
-    return 'Inserted %d fields, device_id=%d, timestamp=%d' % (len(fields), event['device_id'], event['timestamp'])
+        cur = conn.cursor()
+        psycopg2.extras.execute_values(cur, sql_house, data_house, template='(to_timestamp(%s), %s, %s, %s, %s, %s)')
+        conn.commit()
+        cur.close()
+        
+        return 'Inserted %d fields, device_id=%d, timestamp=%d' % (len(fields), event['device_id'], event['timestamp'])
