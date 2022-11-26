12872b3e30f2da3a6a31dce4dc6b461a20712c74
RiceEater126 <yxin2@andrew.cmu.edu>
2022-11-26 15:13:05 -0700

    optimizer part

---
+++ backend/sgmp/optimizer/data_collector.py
@@ -0,0 +1,136 @@
+import csv
+import datetime
+import json
+import time
+from concurrent.futures import thread
+
+import xmltodict
+import requests
+from requests.auth import HTTPDigestAuth
+import threading
+
+
+# This script is used for collecting input data for optimzer. This script will be
+# running all the time, and write the data into files of the same directory.
+class EgaugeInterface():
+
+    def __init__(self, mode=None, endpoint=None, username=None, password=None, t_sample=1, topic=None, clientid=None):
+        # Initializing credentials
+        self.endpoint = endpoint
+        self.mode = mode
+        self.username = username
+        self.password = password
+        self.TOPIC_PUBLISH_EGAUGE = topic
+        self.CLIENT_ID = clientid
+
+        # Initializing parameters
+        self.t_sample = t_sample
+        self.keys = ["A.Battery", "A.SubPanel", "A.GridPower", "A.Solar", "A.EV", "ts"]
+
+    def fetch_egauge_data(self):
+        url = 'http://' + self.endpoint + '/cgi-bin/egauge?ins&tot'
+        try:
+            if self.mode == 'web':
+                resp = requests.get(url, auth=HTTPDigestAuth(self.username, self.password))
+            elif self.mode == 'ip':
+                resp = requests.get(url)
+            else:
+                print('mode type not acceptable')
+                return 'Error egauge: mode type not acceptable'
+
+            resp.raise_for_status()
+            data = self.get_egauge_registers(resp)
+
+        except requests.exceptions.HTTPError as err:
+            print(err)
+            return None
+
+        return data
+
+    # Function to process data from e-gauge and convert to useful power values
+    def processing_egauge_data(self):
+        if self.endpoint is None:
+            print('enpoint is None')
+            return 'Error egauge: endpoint is none'
+
+        power_values = dict.fromkeys(self.keys, None)
+        data_ini = self.fetch_egauge_data()
+        time.sleep(self.t_sample)
+        data_end = self.fetch_egauge_data()
+        ts_delta = data_end['ts'] - data_ini['ts']
+
+        try:
+            for i in power_values:
+                if i == 'ts':
+                    power_values['ts'] = datetime.datetime.fromtimestamp(int(data_end['ts'])).strftime(
+                        '%Y-%m-%d %H:%M:%S')
+                else:
+                    power_values[i] = round(((data_end[i] - data_ini[i]) / ts_delta) / 1000, 3)
+
+            # json_dict = json.dumps(power_values)
+            # print('Data Dict: ', json_dict)
+            return power_values
+        except Exception as e:
+            print('Error retrieving data from E-Gauge API: ', e)
+            return ('Error egauge: mode type not acceptable', e)
+
+    def get_egauge_registers(self, response):
+        power_values = dict.fromkeys(self.keys, None)
+        data = xmltodict.parse(response.text)
+        data_json = json.loads(json.dumps(data))
+        keys_set = self.keys
+        if data_json['data']['ts'] != None:
+            power_values['ts'] = int(data_json['data']['ts'])
+        egauge_vals = data_json['data']['r']
+
+        for i in egauge_vals:
+            if i['@n'] in keys_set:
+                power_values[i['@n']] = int(i['v'])
+
+        return power_values
+
+
+def collect_daily_data(period, mode, user, endpoint, password, filename):
+    f = open(filename, 'a')
+    writer = csv.writer(f)
+    egauge_obj = EgaugeInterface(mode=mode, endpoint=endpoint, username=user, password=password)
+
+    def g_tick():
+        t = time.time()
+        while True:
+            t += period
+            yield max(t - time.time(), 0)
+
+    g = g_tick()
+    while True:
+        # time.sleep(next(g))
+        try:
+            power_values = egauge_obj.processing_egauge_data()
+            writer.writerow(power_values.values())  # write a row to the csv file
+        except Exception as ex:
+            print(ex)
+
+    # close the file
+    f.close()
+
+
+# run the script to collect input data for optimizer every second
+if __name__ == "__main__":
+    user = 'gismolab'
+    password = 'slacGISMO'
+    url240 = 'https://egauge47573.egaug.es/cgi-bin/egauge'
+    mode = 'web'
+    houseB_endpoint = 'egauge47571.egaug.es/'  # house B
+    houseA_endpoint = 'egauge47570.egaug.es/'  # house A
+    period = 4
+
+    # thread_houseA = threading.Thread(target=collect_daily_data,
+    #                                  args=(period, mode, user, houseA_endpoint, password, 'houseA_input.csv'))
+    thread_houseB = threading.Thread(target=collect_daily_data,
+                                     args=(period, mode, user, houseB_endpoint, password, 'houseB_input.csv'))
+
+    # thread_houseA.start()
+    thread_houseB.start()
+
+    # thread_houseA.join()
+    thread_houseB.join()
--- backend/sgmp/routes/optimizer.py
+++ backend/sgmp/routes/optimizer.py
@@ -5,21 +5,66 @@
 #   Add EV arrival/departure model
 #   Add robust MPC
 #   Add PID within the battery command
-import datetime
-import json
+from flask import Blueprint, jsonify
+from utils.auth import require_auth
 import time
-import xmltodict
 import pandas as pd
 import numpy as np
 import cvxpy as cvx
-import requests
-from matplotlib import pyplot as plt
-# to make plots interactive go to PyCharm > Preferences > Python Scientific > disable Show plots in tool window
-from requests.auth import HTTPDigestAuth
+import pathlib
+import threading
+
+from utils.functions import err_json
+
+api_optimizer = Blueprint('optimizer', __name__)
+
+STOP = False
+thread = None
+
+
+@api_optimizer.route('/start', methods=['POST'])
+@require_auth()
+def start_optimizer():
+    status = generate_results()
+    if not status:  # the input data is not enough
+        return err_json('input data is not enough')
+
+    global thread
+    thread = threading.Thread(target=main_optimizer, name='optimizer_thread')
+    thread.start()
+    return jsonify({
+        'status': 'ok'
+    })
+
+
+@api_optimizer.route('/terminate', methods=['POST'])
+@require_auth()
+def terminate_optimizer():
+    global STOP
+    STOP = True
+
+    thread.join()
+    return jsonify({
+        'status': 'ok'
+    })
+
+
+def main_optimizer():
+    do_every(5 * 60, generate_results())
 
 
 def generate_results():
-    df_egauge = pd.read_csv('../../../../Lab_05_17.csv')
+    parent_path = pathlib.Path(__file__).parent.resolve().parent.resolve()
+    path = str(parent_path) + '/optimizer/houseB_input.csv'
+    df_egauge = pd.read_csv(path)
+
+    if len(df_egauge) > 1441:  # delete the data before last 24 hours
+        num = len(df_egauge) - 1441
+        df_egauge.drop(df_egauge.head(num).index, inplace=True)
+        df_egauge.to_csv(path, index=False)
+    elif len(df_egauge) < 1441:  # input data is not enough
+        return False
+
     df_egauge['Timestamp'] = pd.to_datetime(df_egauge['Date & Time'], errors='coerce')
     egauge = df_egauge.set_index('Timestamp')
     egauge.drop(columns='Date & Time', inplace=True)
@@ -40,47 +85,22 @@ def generate_results():
 
     # Data preprocessing
     # Solar:
-    print('NaN within the dataset: ')
-    print(solar.isna().sum())
     solar.fillna(method='ffill', inplace=True)
     solar = solar * (-1.0)
     # Battery:
-    print('NaN within the dataset: ')
-    print(battery.isna().sum())
     battery.fillna(method='ffill', inplace=True)
     battery = battery * (-1)
     # Grid:
-    print('NaN within the dataset: ')
-    print(grid.isna().sum())
     grid.fillna(method='ffill', inplace=True)
     grid = grid * (-1)
     # EV:
-    print('NaN within the dataset: ')
-    print(ev.isna().sum())
     ev.fillna(method='ffill', inplace=True)
     ev = ev * (-1)
     # Load:
-    print('NaN within the dataset: ')
-    print(load.isna().sum())
     load.fillna(method='ffill', inplace=True)
     load = load * (-1)
 
-    plt.figure()
-    plt.plot(solar)
-    plt.plot(battery, 'b')
-    plt.plot(ev, 'g')
-    plt.plot(grid, 'k')
-    plt.plot(load, 'r')
-    plt.legend(['Solar', 'Battery', 'EV', 'Grid', 'Load'])
-    plt.title('Original data')
-    plt.show()
-
     net_load_original = solar[0:1440] + battery[0:1440] + ev[0:1440]
-
-    ## Electricity price
-    # PG&E E-19
-    # demand_charges = [(20, np.concatenate((np.arange(int(16 * 4), int(24 * 4)), np.arange(int(0 * 4), int(6 * 4))))),  (2, np.arange(0, int(24 * 4)))]
-    # energy_charges = [(0, np.arange(int(11 * 4), int(15 * 4))), (0.05, np.concatenate((np.arange(int(9 * 4), int(11 * 4)), np.arange(int(15 * 4), int(17 * 4))))), (0.1, np.arange(int(7 * 4), int(9 * 4))), (0.15, np.concatenate((np.arange(0, int(7 * 4)), np.arange(int(17 * 4), int(24 * 4)))))]
 
     # Optimization formulation
     day = 1  # number of days
@@ -130,11 +150,6 @@ def generate_results():
     ]
 
     P = solar[0:1440] + ev[0:1440]  # taking one day of data
-    # P0 = solar[0:1440] + ev[0:1440] + battery[0:1440] # taking one day of data
-    # plt.figure()
-    # plt.plot(P, 'b')
-    # plt.legend(['Net load'])
-    # plt.show()
 
     objective = energy_charges.reshape((1, energy_charges.size)) @ cvx.reshape(cvx.pos(P.values + c), (T, 1))
     # objective = energy_charges.reshape((1,energy_charges.size)) @ cvx.reshape(cvx.pos(P.values + c - d), (T, 1))
@@ -143,166 +158,13 @@ def generate_results():
     min = cvx.Minimize(objective)
     prob = cvx.Problem(min, constraints=constraints)
     prob.solve(solver=cvx.ECOS)
-    print("Optimization status: ", prob.status)
-    print("Cost: ", prob.value)
 
-    batt = c.value
-    # batt = c.value - d.value
-    soe = Q.value
-
-    plt.figure()
-    plt.plot(P.values, 'b')
-    plt.plot(c.value, 'g')
-    # plt.plot(c.value - d.value, 'g')
-    plt.plot(P.values + c.value, 'k')
-    # plt.plot(P.values + c.value - d.value, 'k')
-    plt.legend(['Load', 'Batt', 'Grid'])
-    plt.show()
-
-    plt.figure()
-    plt.subplot(3, 1, 1)
-    plt.plot(c.value, 'g')
-    # plt.plot(c.value-d.value, 'b')
-    plt.plot(solar[0:1440].values, 'r')
-    plt.plot(ev[0:1440].values, 'b')
-    plt.legend(['Battery Opt', 'Solar', 'Load'])
-    plt.ylabel('kW')
-    # plt.subplot(4,1,2)
-    # plt.figure()
-    # plt.plot(Q.value, 'b')
-    # plt.legend(['SOE'])
-    # plt.ylabel('kWh')
-    plt.subplot(3, 1, 2)
-    plt.plot(P.values + c.value, 'b')
-    # plt.plot(P.values + c.value -d.value, 'b')
-    plt.legend(['Net Load - Grid [Optimized]'])
-    plt.ylabel('kW')
-    plt.subplot(3, 1, 3)
-    plt.plot(P + battery[0:1440], 'b')
-    plt.plot(battery[0:1440], 'g')
-    plt.legend(['Net Load - Grid [Non-optimized]', 'Battery No-Opt'])
-    plt.ylabel('kW')
-    plt.show()
-
-    # plt.figure()
-    # plt.plot(Q.value, 'b')
-    # plt.legend(['SOE'])
-    # plt.ylabel('kWh')
-    # plt.show()
-
-    net_opt = P.values + c.value
-    original_cost = energy_charges.reshape((1, energy_charges.size)) @ -np.minimum(
-        net_load_original.values.reshape(T, 1), 0)
-    print("Cost WITHOUT opt: ", original_cost[0][0])
-    optimized_cost = energy_charges.reshape((1, energy_charges.size)) @ -np.minimum(net_opt.reshape(T, 1), 0)
-    print('Optimized Cost: ', optimized_cost)
-    print("Cost WITH opt: ", prob.value)
-    savings = -prob.value + original_cost[0][0]
-    print("Percentage savings: ", savings / original_cost[0][0])
+    result = np.around(c.value, 2)
+    print(result)
+    return True
 
 
-class EgaugeInterface():
-
-    def __init__(self, mode=None, endpoint=None, username=None, password=None, t_sample=5, topic=None, clientid=None):
-        # Initializing credentials
-        self.endpoint = endpoint
-        self.mode = mode
-        self.username = username
-        self.password = password
-        self.TOPIC_PUBLISH_EGAUGE = topic
-        self.CLIENT_ID = clientid
-
-        # Initializing parameters
-        self.t_sample = t_sample
-        self.keys = ["A.Battery", "A.SubPanel", "A.GridPower", "A.Solar", "A.EV", "ts"]
-
-    # Function to process data from e-gauge and convert to useful power values
-    def processing_egauge_data(self):
-        if self.endpoint is None:
-            print('enpoint is None')
-            return 'Error egauge: endpoint is none'
-        self.url = 'http://' + self.endpoint + '/cgi-bin/egauge?ins&tot'
-        power_values = dict.fromkeys(self.keys, None)
-        try:
-            if self.mode == 'web':
-                resp = requests.get(self.url, auth=HTTPDigestAuth(self.username, self.password))
-            elif self.mode == 'ip':
-                resp = requests.get(self.url)
-            else:
-                print('mode type not acceptable')
-                return 'Error egauge: mode type not acceptable'
-
-            resp.raise_for_status()
-            data_ini = self.get_egauge_registers(resp)
-
-        except requests.exceptions.HTTPError as err:
-            print(err)
-            return json.dumps(power_values)
-
-        time.sleep(self.t_sample)
-
-        try:
-            if self.mode == 'web':
-                resp = requests.get(self.url, auth=HTTPDigestAuth(self.username, self.password))
-                resp.raise_for_status()
-            elif self.mode == 'ip':
-                resp = requests.get(self.url)
-                resp.raise_for_status()
-            else:
-                print('mode type not acceptable')
-                return 'Error egauge: mode type not acceptable'
-
-            data_end = self.get_egauge_registers(resp)
-
-        except requests.exceptions.HTTPError as err:
-            print(err)
-            return json.dumps(power_values)
-
-        ts_delta = data_end['ts'] - data_ini['ts']
-        try:
-            for i in power_values:
-                if i == 'ts':
-                    power_values['ts'] = datetime.datetime.fromtimestamp(int(data_end['ts'])).strftime(
-                        '%Y-%m-%d %H:%M:%S')
-                else:
-                    power_values[i] = round(((data_end[i] - data_ini[i]) / ts_delta) / 1000, 3)
-
-            # json_dict = json.dumps(power_values)
-            # print('Data Dict: ', json_dict)
-            return power_values
-        except Exception as e:
-            print('Error retrieving data from E-Gauge API: ', e)
-            return ('Error egauge: mode type not acceptable', e)
-
-    def get_egauge_registers(self, response):
-        power_values = dict.fromkeys(self.keys, None)
-        data = xmltodict.parse(response.text)
-        data_json = json.loads(json.dumps(data))
-        keys_set = self.keys
-        if data_json['data']['ts'] != None:
-            power_values['ts'] = int(data_json['data']['ts'])
-        egauge_vals = data_json['data']['r']
-
-        for i in egauge_vals:
-            if i['@n'] in keys_set:
-                power_values[i['@n']] = int(i['v'])
-
-        return power_values
-
-def do_every(period,f,**kwargs):
-    def g_tick():
-        t = time.time()
-        while True:
-            t += period
-            yield max(t - time.time(),0)
-    g = g_tick()
-    while True:
-        time.sleep(next(g))
-        print(next(g))
-        f(**kwargs)
-
-def collect_daily_data(period):
-    egauge_obj = EgaugeInterface(mode=mode, endpoint=endpoint, username=user, password=password)
+def do_every(period, f):
     def g_tick():
         t = time.time()
         while True:
@@ -312,11 +174,12 @@ def collect_daily_data(period):
     g = g_tick()
     while True:
         time.sleep(next(g))
-        data = egauge_obj.processing_egauge_data()
+        f()
+        if STOP:
+            break
 
-def write_to_file(filename):
-    pass
 
+# test the optimizer
 if __name__ == "__main__":
     user = 'gismolab'
     password = 'slacGISMO'
@@ -324,6 +187,5 @@ if __name__ == "__main__":
     mode = 'web'
     endpoint = 'egauge47571.egaug.es/'  # house B
     # endpoint = egauge47570.egaug.es/ - house A
-    # print(EgaugeInterface(mode=mode, endpoint=endpoint, username=user, password=password).processing_egauge_data())
-    # do_every(4, collect_daily_data(), 1)
-    generate_results()
+
+    # generate_results()
