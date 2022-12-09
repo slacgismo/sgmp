f40eff0b3ced46ac6e5b2ca8203b75c2eb75d2ce
Nira7 <nikitami@andrew.cmu.edu>
2022-12-09 11:49:25 -0800

    Updates in topic and config

--- backend/sgmp/optimizer/publishSonnen.py
+++ backend/sgmp/optimizer/publishSonnen.py
@@ -1,4 +1,4 @@
-import config
+import utils.config as config
 import AWSIoTPythonSDK.MQTTLib as AWSIoTPyMQTT
 import json
 
@@ -8,18 +8,17 @@ IOT_CLIENT_ID = config.IOT_CLIENT_ID
 IOT_CERT_PATH = config.IOT_CERT_PATH
 IOT_KEY_PATH = config.IOT_KEY_PATH
 IOT_ROOT_PATH = config.IOT_ROOT_PATH
-TOPIC_CONTROL_SONNEN = config.TOPIC_CONTROL_SONNEN
+TOPIC_CONTROL = config.DEPLOYMENT_NAME + '_write/'
 
-# Publish output of the optimizer algorithm to an MQTT control topic for specific house
 def publishPowerValues(publishMessage):
     # initialize client:
     myAWSIoTMQTTClient = AWSIoTPyMQTT.AWSIoTMQTTClient(IOT_CLIENT_ID)
     myAWSIoTMQTTClient.configureEndpoint(IOT_ENDPOINT, 8883)
     myAWSIoTMQTTClient.configureCredentials(IOT_ROOT_PATH, IOT_KEY_PATH, IOT_CERT_PATH)
-    
+
     mqtt_connect = False
     while not(mqtt_connect):
         mqtt_connect = myAWSIoTMQTTClient.connect()
         print("Connected to AWS IoT client: ", mqtt_connect)
-
-    myAWSIoTMQTTClient.publish(TOPIC_CONTROL_SONNEN + publishMessage["DeviceID"], json.dumps(publishMessage), QoS=1)
+  
+    myAWSIoTMQTTClient.publish(TOPIC_CONTROL + publishMessage["DeviceID"] + '/battery', json.dumps(publishMessage), QoS=1)
